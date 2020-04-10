bcb006a234c3b12c4852bed7093c2399c7e4d1be
jpsant <j.paaulinho@gmail.com>
2020-04-10 17:11:08 -0300

    adding the arrow-buttons of the mobile sections navigation

--- src/components/MobileMenu/index.js
+++ src/components/MobileMenu/index.js
@@ -5,6 +5,9 @@ import VectorMobileUp from '../../assets/svg/vector-mobile-up.svg';
 import VectorMobileDownRight from '../../assets/svg/vector-mobile-down-right.svg';
 import VectorMobileDownLeft from '../../assets/svg/vector-mobile-down-left.svg';
 
+import ArrowButtonLeft from '../../assets/svg/mobile-arrow-left.svg';
+import ArrowButtonRight from '../../assets/svg/mobile-arrow-right.svg';
+
 export default function MobileMenu({ currentPage, aboutHandler, skillsHandler }) {
   const [mobileMenu, handleMobileMenu] = useState(false);
 
@@ -18,13 +21,19 @@ export default function MobileMenu({ currentPage, aboutHandler, skillsHandler })
         <button onClick={() => handleMobileMenu(!mobileMenu)} className="mobile-menu-button">press me!</button>
       </div>
 
-      <button onClick={ currentPage === 1 ? () =>  aboutHandler(-1) 
-      : currentPage === 2 ? () => skillsHandler(-1) : null} 
-      className="mobile-menu-button-left">left</button>
+      <button onClick={currentPage === 1 ? () => aboutHandler(-1)
+        : currentPage === 2 ? () => skillsHandler(-1) : null}
+        className="mobile-menu-button-left"
+        style={{ opacity: currentPage === 0 ? "0" : currentPage === 3 ? "0" : "1" }}>
+        <img src={ArrowButtonLeft} alt="Mobile Arrow Left" />
+      </button>
 
-      <button onClick={ currentPage === 1 ? () =>  aboutHandler(1) 
-      : currentPage === 2 ? () => skillsHandler(1) : null}
-      className="mobile-menu-button-right">right</button>
+      <button onClick={currentPage === 1 ? () => aboutHandler(1)
+        : currentPage === 2 ? () => skillsHandler(1) : null}
+        className="mobile-menu-button-right"
+        style={{ opacity: currentPage === 0 ? "0" : currentPage === 3 ? "0" : "1" }}>
+        <img src={ArrowButtonRight} alt="Mobile Arrow Right" />
+      </button>
 
       <img style={{ opacity: currentPage === 0 ? "0" : currentPage === 3 ? "0" : "1" }} src={VectorMobileDownRight} alt="Mobile Down Right Vector Shape" className="shape-down-right" />
       <img style={{ opacity: currentPage === 0 ? "0" : currentPage === 3 ? "0" : "1" }} src={VectorMobileDownLeft} alt="Mobile Down Vector Left Shape" className="shape-down-left" />
