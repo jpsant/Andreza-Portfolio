ada97b989f1440bf37b48c9266172a490feeb5fb
João Paulo <j.paaulinho@gmail.com>
2020-06-25 19:32:48 -0300

    created a file that contains all the textfiles of the aplication (english/portuguese)

---
+++ src/multiLanguages/languages/english.js
@@ -0,0 +1,31 @@
+let english = {
+
+  menu: {
+    home: 'Home',
+    about: 'About me',
+    specialties: 'Specialties',
+    contact: 'Contact'
+  },
+
+  homeSection: {
+    profession: 'Veterinarian' 
+  },
+
+  skillsSection: {
+    title: 'Specialties'
+  },
+
+  contactSection: {
+    title: 'Contact me',
+    social: 'Follow me on social media:'
+  },
+
+  contactForm: {
+    email: 'Your email',
+    subject: 'Subject',
+    message: 'Mensage',
+    send: 'Send'
+  }
+}
+
+export default english;
---
+++ src/multiLanguages/languages/portuguese.js
@@ -0,0 +1,31 @@
+let portuguese = {
+
+  menu: {
+    home: 'Inicio',
+    about: 'Sobre mim',
+    specialties: 'Especialidades',
+    contact: 'Contato'
+  },
+
+  homeSection: {
+    profession: 'Médica Veterinária' 
+  },
+
+  skillsSection: {
+    title: 'Especialidades'
+  },
+
+  contactSection: {
+    title: 'Entre em contato',
+    social: 'Siga-me nas redes sociais:'
+  },
+
+  contactForm: {
+    email: 'Seu email',
+    subject: 'Assunto',
+    message: 'Mensagem',
+    send: 'Enviar'
+  }
+}
+
+export default portuguese;
